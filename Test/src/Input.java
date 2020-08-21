a14474416c2301a65c90134f14a43513146b34a7
Youngjin Yang <ojin0611@gmail.com>
2020-08-21 16:25:34 +0900

    input, output 수정필요

--- Test/src/Input.java
+++ Test/src/Input.java
@@ -1,11 +1,47 @@
+import java.util.Scanner;
 import java.util.Vector;
-
 public class Input {
-
-	public Input(Vector<Employee> employee) {
+	private Vector<Employee> vector;
+	private Scanner scan;
+	Input(Vector<Employee> vector){
+		this.vector = vector;
+		this.scan = new Scanner(System.in);
 	}
-
-	public void input() {
+	public	void input(){
+		String y_n = null;   //초기화
+		do{
+			System.out.print("사원번호 : ");
+			int no = this.scan.nextInt();
+			
+			String stringNo = Integer.toString(no);
+			if(stringNo.length() > 2) {
+				System.out.println("사원번호는 정수 2자리 이내로 입력해주세요.");
+				continue;
+			}
+			
+			
+			System.out.print("급 : ");
+			int level = this.scan.nextInt();
+			String stringLevel = Integer.toString(level);
+			if(stringLevel.length() > 1) {
+				System.out.println("급, 호는 정수 1자리 이내로 입력해주세요.");
+				continue;
+			}
+			
+			System.out.print("호 : ");
+			int ho = this.scan.nextInt();
+			String stringHo = Integer.toString(ho);
+			if(stringHo.length() > 1) {
+				System.out.println("급, 호는 정수 1자리 이내로 입력해주세요.");
+				continue;
+			}
+			
+			System.out.print("수당 : ");
+			int sudang = this.scan.nextInt();
+			System.out.print("입력 / 출력 (I/O) ? :");
+			y_n = this.scan.next().trim();
+			Employee employee = new Employee(no, level, ho, sudang);
+			this.vector.addElement(employee);
+		}while(y_n.toUpperCase().equals("I"));
 	}
-
 }
--- Test/src/Output.java
+++ Test/src/Output.java
@@ -1,11 +1,46 @@
 import java.util.Vector;
-
 public class Output {
-
-	public Output(Vector<Employee> employee) {
-	}
-
-	public void output() {
-	}
-
-}
+    private Vector<Employee> vector;
+    public Output(Vector<Employee> vector) {
+        this.vector = vector;
+    }
+    
+    void output() {
+        System.out.println("\t\t\t\t<<급여관리 프로그램>>");
+        this.printLine();
+        System.out.println("사번\t급수\t호\t수당\t지급액\t세금\t차인지급액");
+        this.printLine();
+        
+        for(int i=0; i<this.vector.size(); i++) {
+            Employee p = this.vector.elementAt(i);
+            //no(사번), level(급수), ho(호봉), sudang(수당), paid(지급액), tax(세금), realPaid(차인지급액)
+            System.out.printf(
+                    "%d\t%d\t%d\t%d\t%d\t%d\t%d",
+                    p.getNo(), p.getLevel(), p.getHo(), p.getSudang(), p.getPaid(), p.getTax(), p.getRealPaid()
+                    );
+        }
+        
+        this.printLine();
+        System.out.println();
+        System.out.println("입력데이타 출력");
+        for(int i=0; i<this.vector.size(); i++) {
+            Employee p = this.vector.elementAt(i);
+            System.out.printf(
+                    "%d\t%d\t%d\t%d",
+                    p.getNo(), p.getLevel(), p.getHo(), p.getSudang()
+                    );
+        }
+        
+    }//output
+    
+    
+    //그냥 라인입니다.
+    private void printLine() {
+        for(int i=0; i<70; i++) {
+            System.out.print("-");
+        }
+    }
+    
+    
+    
+}//Output 
