84937779fbad4f526eac656bfe7c8c86ce7f7727
user <user@DESKTOP-COJ9858>
2020-08-21 16:44:03 +0900

    완료

--- Test/src/Output.java
+++ Test/src/Output.java
@@ -9,14 +9,14 @@ public class Output {
         System.out.println("\t\t<<급여관리 프로그램>>");
         this.printLine();
         //System.out.println("사번\t급수\t호\t수당\t지급액\t세금\t차인지급액");
-        System.out.printf("%4s%4s%5s%3s%2s%1s%s\n","사번", "급수", "호", "수당", "지급액", "세금", "차인지급액");
+        System.out.printf("%5s%5s%5s%7s%7s%5s%10s\n","사번", "급수", "호", "수당", "지급액", "세금", "차인지급액");
         this.printLine();
         
         for(int i=0; i<this.vector.size(); i++) {
             Employee p = this.vector.elementAt(i);
             //no(사번), level(급수), ho(호봉), sudang(수당), paid(지급액), tax(세금), realPaid(차인지급액)
             System.out.printf(
-                    "%5d\t%5d\t%5d\t%5d\t%5d\t%5d\t%5d",
+                    "%5d\t%5d\t%5d\t%7d\t%7d\t%5d\t%10d\n",
                     p.getNo(), p.getLevel(), p.getHo(), p.getSudang(), p.getPaid(), p.getTax(), p.getRealPaid()
                     );
         }
@@ -24,11 +24,11 @@ public class Output {
         System.out.println();
         this.printLine();
         System.out.println();
-        System.out.println("입력데이타 출력");
+        System.out.println("\n\n입력데이타 출력");
         for(int i=0; i<this.vector.size(); i++) {
             Employee p = this.vector.elementAt(i);
             System.out.printf(
-                    "%d\t%d\t%d\t%d",
+                    "%5d\t%5d\t%5d\t%8d%n",
                     p.getNo(), p.getLevel(), p.getHo(), p.getSudang()
                     );
         }
@@ -38,7 +38,7 @@ public class Output {
     
     //그냥 라인입니다.
     private void printLine() {
-        for(int i=0; i<60; i++) {
+        for(int i=0; i<64; i++) {
             System.out.print("-");
         }
         System.out.println();
