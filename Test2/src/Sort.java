5854e246bd81c5a6a6dbf940b3e392a226030edb
Youngjin Yang <ojin0611@gmail.com>
2020-08-28 16:43:11 +0900

    Employee 완

--- Test2/src/Sort.java
+++ Test2/src/Sort.java
@@ -1,11 +1,17 @@
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.ArrayList;
 
 public class Sort {
-
+	private ArrayList<Employee> employee;
 	public Sort(ArrayList<Employee> employee) {
+		this.employee = employee;
 	}
-
-	public void sort() {
+	void sort() {
+		Collections.sort(this.employee, (o1, o2) ->((o1.getEmployeeNo() > o2.getEmployeeNo()) ? -1 :
+			(o1.getEmployeeNo() < o2.getEmployeeNo()) ? 1 : 0)*-1);
 	}
-
 }
+
+/*
+*/
